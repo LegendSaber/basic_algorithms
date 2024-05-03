9b6a0a56d82d49177541ebcf1edee1ae9ab9102e
LegendSaber <747671402@qq.com>
2024-05-03 17:09:55 +0800

    快速排序

--- src/sort.rs
+++ src/sort.rs
@@ -46,9 +46,39 @@ fn merge_sort(a: &mut [i32], begin: usize, end: usize) {
     }
 }
 
+fn partition(a: &mut [i32], begin: usize, end: usize) -> usize {
+    let flag = a[begin];
+    let mut begin = begin;
+    let mut end = end;
+
+    while begin < end {
+        while a[end] > flag && begin < end {
+            end -= 1;
+        }
+        a[begin] = a[end];
+
+        while a[begin] < flag && begin < end {
+            begin += 1;
+        }
+        a[end] = a[begin];
+    }
+
+    a[begin] = flag;
+
+    begin
+}
+
+fn quick_sort(a: &mut [i32], begin: usize, end: usize) {
+    if begin < end {
+        let q = partition(a, begin, end);
+        quick_sort(a, begin, q);
+        quick_sort(a, q + 1, end);
+    }
+}
+
 #[cfg(test)]
 mod test{
-    use crate::sort::merge_sort;
+    use crate::sort::{merge_sort, quick_sort};
 
     #[test]
     fn test_merge() {
@@ -56,6 +86,15 @@ mod test{
         let len = a.len() - 1;
 
         merge_sort(&mut a, 0, len);
+        assert_eq!(a, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    }
+
+    #[test]
+    fn test_quick() {
+        let mut a = [3, 7, 6, 1, 2, 4, 5, 9, 8];
+        let len = a.len() - 1;
+
+        quick_sort(&mut a, 0, len);
         assert_eq!(a, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
     }
 }
